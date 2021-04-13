b9b2883478fa23b28742f6dca0becd55868b6f05
TMats <takomats@hotmail.com>
2021-04-12 23:30:12 -0400

    installed npm package

---
+++ Main/index.js
@@ -0,0 +1,4 @@
+const Employee = require("../Develop/lib/Employee");
+const Engineer = require("../Develop/lib/Engineer");
+const Intern = require("../Develop/lib/Intern");
+const Manager = require("../Develop/lib/Manager");
